13b48d0f2166b9e49224c42b94e91b58c54b9d5b
Fauzan R <fauzanrafi.upraktik.24@gmail.com>
2021-09-16 16:30:04 +0700

    add line

--- pemvis1/src/App.java
+++ pemvis1/src/App.java
@@ -1,17 +1,114 @@
 // author fauzan,khamim,dafa,inas
 
 import java.awt.Color;
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 
-public class App extends JFrame implements ActionListener{
+public class App extends JFrame {
 
     //initiate componen
     private JPanel mainPanel,aPanel,bPanel,cPanel,dPanel,ePanel;
     private JButton kembaliButton,lanjutButton,aButton,bButton,cButton,dButton,eButton;
     private JLabel aPanelLabel,bLabelPanel,cLabelPanel,dLabelPanel,eLabelPanel,judulLabel;
     private JSeparator jSeparator;
+
+    // function to set layout
+    private void initComponents(){
+        judulLabel = new JLabel();
+        aButton = new JButton();
+        bButton = new JButton();
+        cButton = new JButton();
+        dButton = new JButton();
+        eButton = new JButton();
+        jSeparator = new JSeparator();
+        mainPanel = new JPanel();
+        aPanel = new JPanel();
+        aPanelLabel = new JLabel();
+        bPanel = new JPanel();
+        bLabelPanel = new JLabel();
+        cPanel = new JPanel();
+        cLabelPanel = new JLabel();
+        dPanel = new JPanel();
+        dLabelPanel = new JLabel();
+        ePanel = new JPanel();
+        eLabelPanel = new JLabel();
+        lanjutButton = new JButton();
+        kembaliButton = new JButton();
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("COntoh Swing Panel 1 Frame");
+
+        judulLabel.setFont(new java.awt.Font("Ubuntu",1,15));
+        judulLabel.setText("Contoh Swing Panel 1");
+
+        // init component button
+        aButton.setText("Panel A");
+        aButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                aButtonActionPerformed(evt);
+            }
+        });
+
+        bButton.setText(">> Panel B");
+        bButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bButtonActionPerformed(evt);
+            }
+        });
+
+        cButton.setText(">> Panel C");
+        cButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cButtonActionPerformed(evt);
+            }
+        });
+
+        dButton.setText(">> Panel D");
+        dButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                dButtonActionPerformed(evt);
+            }
+        });
+
+        eButton.setText(">> Panel E");
+        eButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                eButtonActionPerformed(evt);
+            }
+        });
+
+        kembaliButton.setFont(new java.awt.Font("Ubuntu", 1, 15));
+        kembaliButton.setText("<<");
+        kembaliButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                kembaliButtonActionPerformed(evt);
+            }
+        });
+
+        lanjutButton.setFont(new java.awt.Font("Ubuntu", 1, 15));
+        lanjutButton.setText(">>");
+        lanjutButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                lanjutButtonActionPerformed(evt);
+            }
+        });
+
+        jSeparator.setBackground(new Color(247, 247, 247));
+
+        mainPanel.setLayout(new CardLayout());
+
+        aPanel.setBackground(new Color(240, 240, 240));
+        aPanelLabel.setFont(new java.awt.Font("Ubuntu", 1, 24)); // NOI18N
+        aPanelLabel.setForeground(new java.awt.Color(255, 255, 255));
+        aPanelLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        aPanelLabel.setText("Panel A");
+
+        GroupLayout aPanelLayout = new GroupLayout(aPanel);
+
+        
+    }
 
     // initiate array for store button,panel and color
     JPanel[] arrPanel;
@@ -61,11 +158,43 @@ public class App extends JFrame implements ActionListener{
         }
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e){
-        
+
+    // function button --------------------------------
+    private void aButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        panelPilihan = 0;
+        setTampil();
+    }       
+
+    private void bButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        panelPilihan = 1;
+        setTampil();
     }
-    
+
+    private void cButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        panelPilihan = 2;
+        setTampil();
+    }
+
+    private void dButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        panelPilihan = 3;
+        setTampil();
+    }
+
+    private void eButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        panelPilihan = 4;
+        setTampil();
+    }
+
+    private void kembaliButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        panelPilihan -= 1;
+        setTampil();
+    }
+
+    private void lanjutButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        panelPilihan += 1;
+        setTampil();
+    }
+    //----------------------------------------------
 
     public App() {
         
